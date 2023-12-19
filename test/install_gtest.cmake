0f3a4f66a45577d20ba2c99dd34644fb235f8c37
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-19 13:34:08 +0100

    minor refactor

--- test/install_gtest.cmake
+++ test/install_gtest.cmake
@@ -16,30 +16,18 @@ add_library(libgtest IMPORTED STATIC GLOBAL)
 add_dependencies(libgtest gtest)
 
 # Set libgtest properties
-if(WIN32)
-    set_target_properties(libgtest PROPERTIES
-        "IMPORTED_LOCATION" "${binary_dir}/lib/Debug/gtestd.lib"
-        "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}")
-else()
-    set_target_properties(libgtest PROPERTIES
-        "IMPORTED_LOCATION" "${binary_dir}/lib/libgtest.a"
-        "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}")
-endif(WIN32)
+set_target_properties(libgtest PROPERTIES
+	"IMPORTED_LOCATION" "${binary_dir}/lib/libgtest.a"
+	"IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}")
 
 # Create a libgmock target to be used as a dependency by test programs
 add_library(libgmock IMPORTED STATIC GLOBAL)
 add_dependencies(libgmock gtest)
 
 # Set libgmock properties
-if(WIN32)
-    set_target_properties(libgmock PROPERTIES
-        "IMPORTED_LOCATION" "${binary_dir}/lib/Debug/gmockd.lib"
-        "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}")
-else()
-    set_target_properties(libgmock PROPERTIES
-        "IMPORTED_LOCATION" "${binary_dir}/lib/libgmock.a"
-        "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}")
-endif(WIN32)
+set_target_properties(libgmock PROPERTIES
+	"IMPORTED_LOCATION" "${binary_dir}/lib/libgmock.a"
+	"IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}")
 
 # I couldn't make it work with INTERFACE_INCLUDE_DIRECTORIES
 include_directories("${source_dir}/googletest/include"
