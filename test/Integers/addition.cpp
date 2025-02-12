e540cb835f3d1927ba67626245d78fe959cda81d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-12 21:11:28 +0100

    basic test

---
+++ test/Integers/addition.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+extern "C"
+{
+#include "integers/integers.h"
+};
+
+TEST(Addition, PositiveNumbers)
+{
+    int expected = 3 + 5;
+    int actual = add(3, 5);
+
+    ASSERT_EQ(expected, actual) << "Error, expected " << expected << " was " << actual;
+}
